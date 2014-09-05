57139cb45136fd9807d104984c2df362c636022c
Jonathan Lisic <jlisic@gmail.com>
2014-09-04 23:30:19 -0400

    working now

--- flannR.R
+++ flannR.R
@@ -11,7 +11,11 @@
   stop( sprintf("Invalid Algorithm = %s",param) )
 }
 
-
+.returnKernelEnum <- function( param ) {
+  if( param == "NONE" )    return( 0 );
+  if( param == "UNIFORM" ) return( 1 );
+  if( param == "NORMAL" )  return( 2 );
+}
 
 
 ############################################################
@@ -24,7 +28,9 @@ getNN <- function(
   nNeighbors,       # number of neighbors
   indexParameters = NULL,   # index parameters  
   searchParameters = NULL,  # search parameters 
-  algorithm = "FLANN_INDEX_KDTREE"
+  algorithm = "FLANN_INDEX_KDTREE",
+  kernelMethod = "NONE",
+  bandwidth = NULL
   ) {
   
   # default search parameters
@@ -49,7 +55,15 @@ getNN <- function(
 
   # get an algorithm
   paramAlgorithm <- .flannAlgorithmEnum( algorithm )
- 
+
+  # get kernel method
+  paramKernelMethod <- .returnKernelEnum( kernelMethod )
+
+  if( kernelMethod == "NORMAL" ) {
+    if( is.null(bandwidth) ) stop("bandwidth NULL for kernel method")
+    if( length(bandwidth) != queryCol ) stop("bandwidth not equal to columns x")
+  }
+
   # load shared libarary 
   dyn.load('~/src/dissertation/bin/flannR.so')
   #dyn.load('~/src/dissertation/src/flannR/flannR.so')
@@ -59,19 +73,21 @@ getNN <- function(
 
   # send our data to the C program
   r.result <- .C("R_meanShiftNN",
-    as.double( t(queryData) ),        # 1 data we query
-    as.double( t(trainData) ),        # 2 sample we use to build index
-    as.integer( neighbors ),          # 3
-    as.double(  distances ),          # 4
-    as.integer( queryRow ),           # 5 number of rows of data to query
-    as.integer( trainRow ),           # 5 number of rows of training data 
-    as.integer( queryCol ),           # 6 number of columns of data to query
-    as.integer( nNeighbors ),         # 7 number of Neighbors 
+    as.double( t(queryData) ),                 # 1 data we query
+    as.double( t(trainData) ),                 # 2 sample we use to build index
+    as.integer( neighbors ),                   # 3
+    as.double(  distances ),                   # 4
+    as.integer( queryRow ),                    # 5 number of rows of data to query
+    as.integer( trainRow ),                    # 5 number of rows of training data 
+    as.integer( queryCol ),                    # 6 number of columns of data to query
+    as.integer( nNeighbors ),                  # 7 number of Neighbors 
     as.integer( integerSearchParameters ),     # 8 integer Parameters for searching
     as.double( doubleSearchParameters ),       # 9 double Parameters  for searching
     as.integer( integerAlgorithmParameters ),  # 10 integer Parameters for algorithm 
     as.double( doubleAlgorithmParameters ),    # 11 double Parameters for algorithm
-    as.integer( paramAlgorithm )               # 12 method to form tree
+    as.integer( paramAlgorithm ),              # 12 method to form tree
+    as.integer( paramKernelMethod ),           # 13 method to form tree
+    as.double( bandwidth )                     # 14 method to form tree
   )
   
   print("C running time")
@@ -103,10 +119,19 @@ if( "testShiftNN" %in% ls() ) {
                4, 5 ), byrow=T, ncol=2)
 
 
-    x1 <- matrix( rnorm( 10000 ),ncol=2)
-    x2 <- matrix( rnorm( 10000 ),ncol=2) + 100 
+#    x1 <- matrix( rnorm( 10000 ),ncol=2)
+#    x2 <- matrix( rnorm( 10000 ),ncol=2) + 100 
+
+    x1 <- x
+    x2 <- x + 30
     
     x <- rbind( x1, x2 ) 
+
+    nn <- getNN(x, x[4:9,], 
+                3,
+                kernelMethod = "NORMAL", bandwidth=c(3,3) 
+               ) 
+     print(nn)
 
     library(raster)
     r <- raster("~/src/dissertation/src/rSegmentation/test.gri")
@@ -118,9 +143,9 @@ if( "testShiftNN" %in% ls() ) {
     2064731
     )
 
-    r <- crop(r, extent(Ext) )  
+    #r <- crop(r, extent(Ext) )  
 
-    source('../cSmooth/smooth.R')
+    source('~/src/dissertation/src/cSmooth/smooth.R')
     h <- 17 
     WMean   <- matrix(1/h^2,nrow=h,ncol=h)
     WVar    <- matrix(1/h^2,nrow=h,ncol=h)
@@ -128,46 +153,60 @@ if( "testShiftNN" %in% ls() ) {
 
 
     r.values <- cbind(
-      rep(1:ncol(r$mu),nrow(r$mu)),
-      rep(1:nrow(r$mu),each=ncol(r$mu)),
+#      rep(1:ncol(r$mu),nrow(r$mu)),
+#      rep(1:nrow(r$mu),each=ncol(r$mu)),
       values(r$mu),
       values(log(r$var))
       )
 
-    r.values.finite <- is.finite(rowSums(r.values)) 
-
-    x <- r.values[ r.values.finite, ]
+     r.values.finite <- is.finite(rowSums(r.values)) 
+ 
+     x <- r.values[ r.values.finite, ]
     
-    index <- sample( 1:nrow(x), 10 ) 
-    train <- x[index,]
-   
-    nn <- getNN(train, train, 5)
-    print( nn$query ) 
-
-    for( i in 1:10 ) {
-      nn <- getNN(nn$query, train, 5)
-      print( nn$query )
-    }
+     sampleRate <- 5000 
+     #bandwidth <- c(100,100,8,2)
+     bandwidth <- c(8,2)
+ 
+     index <- seq.int(from=1,to=ncell(r$mu),by=sampleRate)
+ 
+     #index <- sample( 1:nrow(x), sampleSize ) 
+     train <- x[index,]
     
+     nn <- getNN(train, train, 
+                 #length(index), 
+                 100,
+                 kernelMethod = "NORMAL" , 
+                 bandwidth=bandwidth)
+ 
+     for( i in 1:20) {
+       nn <- getNN(nn$query, train, 
+                   #length(index), 
+                   100,
+                   kernelMethod="NORMAL", 
+                   bandwidth=bandwidth)
+     }
     
-
-#    # copy the results back
-#    r.new <-r$mu
-#    r.new.values <- values(r.new) 
-#    r.new.values[r.values.finite] <- nn$query[,3]
-#    values(r.new) <- r.new.values
-#    
-#    r.new.var <-r$var
-#    r.new.values <- values(r.new) 
-#    r.new.values[r.values.finite] <- nn$query[,4]
-#    values(r.new.var) <- r.new.values
+     # classify   
+     y <- getNN(x, nn$query, 1)
+     
+ 
+ #    # copy the results back
+     r.new <-r$mu
+     r.new.values <- values(r.new) 
+     r.new.values[r.values.finite] <- y$query[,1]
+     values(r.new) <- r.new.values
+     
+     r.new.var <-r$var
+     r.new.values <- values(r.new) 
+     r.new.values[r.values.finite] <- y$query[,2]
+     values(r.new.var) <- r.new.values
+ 
+     par( mfrow=c(2,2) )
+     plot(r$mu)
+     plot(r.new)
+     plot(log(r$var))
+     plot(r.new.var)
 #
-#    par( mfrow=c(2,2) )
-#    plot(r$mu)
-#    plot(r.new)
-#    plot(log(r$var))
-#    plot(r.new.var)
-
 #    nCheck <- 2:4
 #    
 #    y <- x[nCheck,]
--- flannR.cpp
+++ flannR.cpp
@@ -8,6 +8,119 @@
 #include "omp.h"
 
 
+/* kernel methods */
+
+
+
+
+/***************************** NO KERN *********************************/
+void noneKernel( 
+    double * x, 
+    double * train, 
+    int * neighbors,
+    size_t xRow, 
+    size_t xCol, 
+    size_t nNeighbors 
+    ) {
+
+  size_t i, j, k;
+
+  for( i = 0; i < xRow; i++) {              // rows
+    for( k = 0; k < xCol; k++) {          // dimensions
+      x[ i * xCol + k] = 0;         
+      for( j = 0; j < nNeighbors; j++) {            // neighbors 
+        x[ i * xCol + k ] += train[ neighbors[i* nNeighbors + j] * xCol + k ]/nNeighbors;
+      }
+    }
+  }
+
+  return;
+} 
+
+
+
+
+/************************** NORMAL KERN *********************************/
+void normalKernel(
+  double * query,        /* query matrix */
+  double * build,        /* train matrix */
+  int * neighbors,      /* neighbors matrix */
+  size_t queryRow,
+  size_t queryCol,
+  size_t nNeighbors,       /* number of neighbors */
+  double * bandwidth     /* bandwidth parameters */
+  ) {
+
+  size_t i; /* query data index */
+  size_t j; /* build data index */ 
+  size_t k; /* neighbor index */
+  size_t d; /* column index */
+
+  double * y = (double * ) malloc( sizeof(double) * queryCol);
+  double w;
+  double denominator;
+  double * numerator = (double * ) malloc( sizeof(double) * queryCol);
+  int currentNeighbor;
+  int useWeights = 1; 
+
+  
+  /* iterate over each query point */ 
+  for( i = 0; i < queryRow; i++)  {
+
+applyWeights:
+
+    /* total weight to divide by for the denominator */
+    for( d = 0; d < queryCol; d++)  numerator[d] = 0; 
+    denominator = 0; 
+
+    /* iterate over each neighbor */
+    for( k = 0; k < nNeighbors; k++)  { 
+
+      /* d'th element of the k'th neighbor of the i'th query point */ 
+      currentNeighbor = neighbors[i*nNeighbors + k];
+  
+      /* set weight to 1 */ 
+      w = 1;
+
+      // for each point query over each dimension 
+      for( d = 0; d < queryCol; d++)  { 
+
+        // save a copy of the point 
+        y[d] = build[currentNeighbor * queryCol + d]; 
+        
+        // double dnorm( double x, double mu, double sigma, int give_log)   
+        if( useWeights == 1 )  w *=  dnorm( ( y[d] - query[i * queryCol + d]) , 0, bandwidth[d], 0 );
+
+      }
+     
+      // update denominator and numerator 
+      for( d = 0; d < queryCol; d++)  numerator[d] = numerator[d] + (y[d] * w); 
+      denominator += w; 
+    
+    }
+    
+    
+    /* run it all over again without any weights if you get a 0 denom */
+    if( denominator <= 0 ) {
+      useWeights = 0;
+      goto applyWeights;
+    }
+    useWeights = 1;
+ 
+    // update in place the query set 
+    for( d = 0; d < queryCol; d++) query[i* queryCol + d] = numerator[d] / denominator;
+
+
+  
+  }
+
+  free(y);
+  free(numerator);
+
+  return;
+}
+
+
 
 
 // extern start for .C interface
@@ -16,27 +129,30 @@ extern "C" {
 
 /* mean shfit nearest neighbors */
 void R_meanShiftNN(
-  double * x,                   /* data to query for */
+  double * x,                /* data to query for */
   double * train, 
   int * neighbors,
   double * distances,
-  int * xRowPtr,                /* number of rows of data to query */
-  int * trainRowPtr,            /* number of rows of data to train on */
-  int * xColPtr,                /* number of columns of data to form train */
-  int * nNeighborsPtr,          /* number of Neighbors */
-  int * intSearchParameters,           /* integer parameters for search*/
-  double * doubleSearchParameters,     /* double parameters for search */ 
-  int * intAlgorithmParameters,           /* integer parameters for algorithm*/
-  double * doubleAlgorithmParameters,     /* double parameters for algorithm*/ 
-  int * methodPtr               /* enumerated algorithm type */
+  int * xRowPtr,             /* number of rows of data to query */
+  int * trainRowPtr,         /* number of rows of data to train on */
+  int * xColPtr,             /* number of columns of data to form train */
+  int * nNeighborsPtr,               /* number of Neighbors */
+  int * intSearchParameters,         /* integer parameters for search*/
+  double * doubleSearchParameters,   /* double parameters for search */ 
+  int * intAlgorithmParameters,      /* integer parameters for algorithm*/
+  double * doubleAlgorithmParameters,/* double parameters for algorithm*/
+  int * methodPtr,                   /* enumerated algorithm type */
+  int * kernelMethodPtr,             /* kernel methods */ 
+  double * bandwidth                 /* bandwidth for kernel methods */
 ) {
 
 
   /* simplify the interface by dereferencing the pointers */
-  size_t xRow = (size_t) * xRowPtr; 
+  size_t xRow =     (size_t) * xRowPtr; 
   size_t trainRow = (size_t) * trainRowPtr; 
-  size_t xCol = (size_t) * xColPtr;
-  size_t method = (size_t) * methodPtr;
+  size_t xCol =     (size_t) * xColPtr;
+  size_t method =   (size_t) * methodPtr;
+  size_t kernelMethod =   (size_t) * kernelMethodPtr;
 
   size_t nNeighbors = (size_t) * nNeighborsPtr;
 
@@ -69,15 +185,26 @@ void R_meanShiftNN(
   index.knnSearch(xMatrix, neighborsMatrix, distancesMatrix, nNeighbors, mpSearchParameters); 
 
 
-  for( i = 0; i < xRow; i++) {              // rows
-    for( k = 0; k < xCol; k++) {          // dimensions
-      x[ i * xCol + k] = 0;         
-      for( j = 0; j < nNeighbors; j++) {            // neighbors 
-        x[ i * xCol + k ] += train[ neighbors[i* nNeighbors + j] * xCol + k ]/nNeighbors;
-      }
-    }
+  /* run kernel methods */
+  if( kernelMethod == 0) {
+    noneKernel( x, train, neighbors,  xRow, xCol,  nNeighbors ); 
+  }
+  
+  if( kernelMethod == 1) {
+    noneKernel( x, train, neighbors,  xRow, xCol,  nNeighbors ); 
   }
 
+  if( kernelMethod == 2) {
+    normalKernel(
+      x,        /* query matrix */
+      train,        /* train matrix */
+      neighbors,      /* neighbors matrix */
+      xRow,
+      xCol,
+      nNeighbors,       /* number of neighbors */
+      bandwidth     /* bandwidth parameters */
+      ); 
+  }
 
   return; 
 }
